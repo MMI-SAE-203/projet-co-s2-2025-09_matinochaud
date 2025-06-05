317a1f258f6ea8234b9e3761f26d2c3804e14c94
kilian-breuil <150059078+kilian-breuil@users.noreply.github.com>
2025-06-05 16:11:11 +0200

    Add @astrojs/netlify dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import alpinejs from '@astrojs/alpinejs';
 
 import react from '@astrojs/react';
 import mod from 'astro/zod';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +15,7 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
   output: "server",
-  adapter: node({mode: 'standalone'}),
+  adapter: netlify(),
   experimental: { svg: true, session: true },
   integrations: [alpinejs(), react()]
 });
